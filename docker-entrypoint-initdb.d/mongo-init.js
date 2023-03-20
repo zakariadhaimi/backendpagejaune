ea4154593f7b97ec1151768d0120bbe92514f52d
Zakaria Dhaimi <zakariadhaimi@192.168.1.9>
2023-03-20 00:57:28 +0000

    large file

--- docker-entrypoint-initdb.d/mongo-init.js
+++
@@ -1,6 +0,0 @@
-db = db.getSiblingDB('pagejaunedb');
-
-
-db.jobs.insertMany([
-    {"_id":ObjectId("5f332fe3bc20771da8577547"),"jobName":"Coiffure","jobPicture":"coiffure","specialty":[],"createdAt":{"$date":"2020-08-11T23:55:15.944Z"},"updatedAt":{"$date":"2020-08-11T23:55:15.944Z"},"__v":0}
-]);
